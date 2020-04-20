a1f4307d3a395244b184c60b0d31300e0d7be2cc
hwl <609678043@qq.com>
2020-04-20 20:09:04 +0800

    hwl's examination

--- src/alitest/Test1.java
+++ src/alitest/Test1.java
@@ -1,18 +1,45 @@
 package alitest;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Test1 {
     public static void main(String[] args) {
-        int M = (int) Math.pow(10, 9) + 7;
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
+            int coins = 0, maxCoins = 0;
             // 输入部分
-            int n = sc.nextInt();
-            int k = sc.nextInt();
-
-            System.out.println("Hello Good Good!hhh");
+            int a = sc.nextInt();// 我的初始能力
+            int n = sc.nextInt();// 怪物数量
+            int[] guaiwu = new int[n];// 怪物能力
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < n; ++i) {
+                guaiwu[i] = sc.nextInt();
+                if (map.get(guaiwu[i]) == null)
+                    map.put(guaiwu[i], 1);
+                else
+                    map.put(guaiwu[i], map.get(guaiwu[i]) + 1);
+            }
+            Arrays.sort(guaiwu);
+            int i = 0;
+            while (i < n) {
+                if (a >= guaiwu[i]) {
+                    coins += map.get(guaiwu[i]);
+                    i += map.get(guaiwu[i]);
+                    maxCoins = maxCoins > coins ? maxCoins : coins;
+                } else if (guaiwu[i] - a <= coins) {
+                    coins = coins + map.get(guaiwu[i]) - (guaiwu[i] - a);
+                    a = guaiwu[i];
+                    i += map.get(guaiwu[i]);
+                    maxCoins = maxCoins > coins ? maxCoins : coins;
+                } else{
+                    break;
+                }
+            }
+            System.out.println(maxCoins);
         }
     }
-
 }
--- src/alitest/Test2.java
+++ src/alitest/Test2.java
@@ -1,17 +1,48 @@
 package alitest;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Test2 {
+    // 找路径
+    public static void findPath(int[][] path, int curPath, int start, int distict) {
+        for(int i = 0; i < path.length; ++i){
+            if(i == start)
+                continue;
+            if(path[start][i] == 1)
+                findPath(path,curPath+1, i,distict);
+        }
+    }
+
     public static void main(String[] args) {
-        int M = (int) Math.pow(10, 9) + 7;
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
+            int path = 0, minPath = 0;
             // 输入部分
-            int n = sc.nextInt();
-            int k = sc.nextInt();
+            int n = sc.nextInt();// 城市个数
+            int[][] lujing = new int[n + 1][n + 1];// 连通图
+            HashMap<Integer, List<Integer>> map = new HashMap<>(); // 保存城市等级
+            for (int i = 0; i < n; ++i) {
+                int pro = sc.nextInt();
+                if (map.get(pro) == null) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(i + 1);
+                    map.put(pro, list);
+                } else {
+                    map.get(pro).add(i + 1);
+                }
+            }
+            for (int i = 0; i < n - 1; ++i) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                lujing[a][b] = 1;
+                lujing[b][a] = 1;
+            }
+            for (int curPro : map.keySet()) {
+                if (map.get(curPro).size()>1){
+                    List<Integer> list = map.get(curPro);
 
-            System.out.println("Hello Good Good!hhh");
+                }
+            }
         }
     }
 }
